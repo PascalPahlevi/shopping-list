3913da3357b4f72b3aea6383c7d0465548b0bb4d
PascalPahlevi <pascalpahlevi@gmail.com>
2023-09-07 10:40:10 +0700

    Tutorial 1 Complete

--- main/views.py
+++ main/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'name': 'Pak Bepe',
-        'class': 'PBP A'
+        'name': 'Muhamad Pascal Alfin Pahlevi',
+        'class': 'PBP Kelas International'
     }
 
     return render(request, 'main.html', context)
